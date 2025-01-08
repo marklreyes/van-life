6595bf575f6ef03ba3e4e18937e2702dc225a330
Mark Reyes <mr@marklreyes.com>
2025-01-08 12:51:49 -0800

    Fetch data for current van, add UI and styles for van details page

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,8 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
 	const params = useParams()
-    console.log(params)
-    return <h1>Van detail page goes here</h1>
+	const [van, setVan] = useState(null);
+
+	useEffect(() => {
+		fetch(`/api/vans/${params.id}`)
+			.then(response => response.json())
+            .then(data => setVan(data.vans))
+			.catch(error => console.error("Error fetching vans data:", error));
+	}, [params.id]);
+    return (
+		<div className="van-detail-container">
+		{van ? (
+			<div className="van-detail">
+				<img src={van.imageUrl} />
+				<i className={`van-type ${van.type} selected`}>{van.type}</i>
+				<h2>{van.name}</h2>
+				<p className="van-price"><span>${van.price}</span>/day</p>
+				<p>{van.description}</p>
+				<button className="link-button">Rent this van</button>
+			</div>
+		) : <h2>Loading...</h2>}
+	</div>
+	)
+
 }
