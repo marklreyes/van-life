99c2e11fe066f42e9cdc16332a6f807880e872bf
Mark Reyes <mr@marklreyes.com>
2025-01-08 15:48:03 -0800

    Remove header from index and use Layout component instead to display the header

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
 
 export default function Layout() {
 	return (
 		<>
-			Layout here...
+			<Header />
+			<Outlet />
 		</>
 	)
 }
