5f04d5023b7e02dfb24b01bdeec014806bad542f
Mark Reyes <mr@marklreyes.com>
2025-01-08 16:28:42 -0800

    Add routes for Dashboard, Income, Reviews pages

--- index.jsx
+++ index.jsx
@@ -6,6 +6,9 @@ import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
 import Layout from './components/Layout';
+import Reviews from './pages/Host/Reviews';
+import Income from './pages/Host/Income';
+import Dashboard from './pages/Host/Dashboard';
 
 import "./server"
 
@@ -18,6 +21,10 @@ function App() {
         <Route path="/about" element={<About />} />
 				<Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
+				<Route path="/host" element={<Dashboard />} />
+				<Route path="/host/income" element={<Income />} />
+				<Route path="/host/reviews" element={<Reviews />} />
+
 				</Route>
       </Routes>
     </BrowserRouter>
