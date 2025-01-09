536a4b40da38c1b6c654da22ab3a5566bc66e2be
Mark Reyes <mr@marklreyes.com>
2025-01-09 11:11:49 -0800

    Add HostLayout component and its own navigation for Dashboard, Income, Reviews

--- index.jsx
+++ index.jsx
@@ -9,6 +9,7 @@ import Layout from './components/Layout';
 import Reviews from './pages/Host/Reviews';
 import Income from './pages/Host/Income';
 import Dashboard from './pages/Host/Dashboard';
+import HostLayout from "./pages/Host/HostLayout"
 
 import "./server"
 
@@ -17,14 +18,15 @@ function App() {
     <BrowserRouter>
       <Routes>
 				<Route element={<Layout />} >
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-				<Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-				<Route path="/host" element={<Dashboard />} />
-				<Route path="/host/income" element={<Income />} />
-				<Route path="/host/reviews" element={<Reviews />} />
-
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/vans" element={<Vans />} />
+					<Route path="/vans/:id" element={<VanDetail />} />
+					<Route path="/" element={<HostLayout />}>
+						<Route path="/host" element={<Dashboard />} />
+						<Route path="/host/income" element={<Income />} />
+						<Route path="/host/reviews" element={<Reviews />} />
+					</Route>
 				</Route>
       </Routes>
     </BrowserRouter>
