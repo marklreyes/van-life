2644db36cc2f5d3165cad2f61b4cc7d852df7eff
Mark Reyes <mr@marklreyes.com>
2025-01-10 13:34:44 -0800

    Add routes for Info, Pricing and Photos using HostVanDetail as the shared layout using Outlet

---
+++ pages/Host/HostVanPhotos.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function HostVanPhoto() {
+	return (
+		<h2>Host Van Photo view here</h2>
+	)
+}
---
+++ pages/Host/HostVanPricing.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function HostVanPricing() {
+	return (
+		<h2>Host Van pricing view here</h2>
+	)
+}
