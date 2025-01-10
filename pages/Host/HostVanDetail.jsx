c3cfa6d89b466871a967ba400eb2597d384a7e16
Mark Reyes <mr@marklreyes.com>
2025-01-10 11:14:12 -0800

    Build the shared UI portion of the Host Van Detail page

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,9 +1,25 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function HostVanDetail() {
+	const { id } = useParams()
+	const [currentVan, setCurrentVan] = React.useState(null)
+
+	React.useEffect(() => {
+		fetch(`/api/host/vans/${id}`)
+			.then(res => res.json())
+			.then(data => setCurrentVan(data.vans))
+	}, [])
+
+	if (!currentVan) {
+		return <h2>Loading...</h2>
+	}
+
 	return (
-	  <>
-		<h1>Host Van Detail page here</h1>
-	  </>
+	  <div>
+		<img src={currentVan.imageUrl} width={150} />
+		<h2>{currentVan.name}</h2>
+		<p>{currentVan.type}</p>
+	  </div>
 	)
 }
