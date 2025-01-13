3f34d9177dbaba06bf5375debffef11eefaeee5c
Mark Reyes <mr@marklreyes.com>
2025-01-13 11:12:07 -0800

    Access the search params in Vans component

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 
 export default function Vans() {
     const [vans, setVans] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams()
+
+	const typeFilter = searchParams.get("type")
+    console.log(typeFilter)
 
     useEffect(() => {
         fetch("/api/vans")
