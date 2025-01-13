02636afa5592c3290cd6ad99763d8c852e083823
Mark Reyes <mr@marklreyes.com>
2025-01-13 14:33:22 -0800

    Conditionally render the className "selected" if the typeFilter value equals the value that button sets it to

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -44,9 +44,9 @@ export default function Vans() {
         <div className="van-list-container">
             <h1>Explore our van options</h1>
 			<div className="van-list-filter-buttons">
-                <button onClick={() => {handleFilterChange("type", "simple")}} className="van-type simple">Simple</button>
-                <button onClick={() => {handleFilterChange("type", "luxury")}} className="van-type luxury">Luxury</button>
-                <button onClick={() => {handleFilterChange("type", "rugged")}} className="van-type rugged">Rugged</button>
+                <button onClick={() => {handleFilterChange("type", "simple")}} className={`van-type simple ${typeFilter === 'simple' ? 'selected' : ''}`}>Simple</button>
+                <button onClick={() => {handleFilterChange("type", "luxury")}} className={`van-type luxury ${typeFilter === 'luxury' ? 'selected' : ''}`}>Luxury</button>
+                <button onClick={() => {handleFilterChange("type", "rugged")}} className={`van-type rugged ${typeFilter === 'rugged' ? 'selected' : ''}`}>Rugged</button>
 				{typeFilter && <button onClick={() => {handleFilterChange("type", null)}} className="van-type clear-filters" >Clear filter</button>}
             </div>
             <div className="van-list">
