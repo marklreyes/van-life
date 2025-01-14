6f3f2eada0ff27d924ae75563c74b1b72d2a3192
Mark Reyes <mr@marklreyes.com>
2025-01-14 12:50:15 -0800

    Set up protected routes for components underneath the host route

---
+++ components/AuthRequired.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Navigate, Outlet } from "react-router-dom"
+
+export default function AuthRequired() {
+    const authenticated = true
+
+    if (!authenticated) {
+        return <Navigate to="/login" />
+    }
+    return <Outlet />
+}
--- index.jsx
+++ index.jsx
@@ -17,6 +17,7 @@ import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPricing from './pages/Host/HostVanPricing';
 import HostVanPhotos from './pages/Host/HostVanPhotos';
 import NotFound from "./pages/NotFound"
+import AuthRequired from "./components/AuthRequired"
 
 import "./server"
 
@@ -30,17 +31,19 @@ function App() {
 					<Route path="vans" element={<Vans />} />
 					<Route path="vans/:id" element={<VanDetail />} />
 					<Route path="login" element={<Login />} />
+					<Route element={<AuthRequired />}>
 					<Route path="host" element={<HostLayout />}>
-						<Route index element={<Dashboard />} />
-						<Route path="income" element={<Income />} />
-						<Route path="vans" element={<HostVans />} />
-						<Route path="vans/:id" element={<HostVanDetail />}>
-							<Route index element={<HostVanInfo />} />
-							<Route path="pricing" element={<HostVanPricing />} />
-							<Route path="photos" element={<HostVanPhotos />} />
-						</Route>
-						<Route path="reviews" element={<Reviews />} />
+					<Route index element={<Dashboard />} />
+					<Route path="income" element={<Income />} />
+					<Route path="reviews" element={<Reviews />} />
+					<Route path="vans" element={<HostVans />} />
+					<Route path="vans/:id" element={<HostVanDetail />}>
+						<Route index element={<HostVanInfo />} />
+						<Route path="pricing" element={<HostVanPricing />} />
+						<Route path="photos" element={<HostVanPhotos />} />
 					</Route>
+					</Route>
+				</Route>
 					<Route path="*" element={<NotFound />}/>
 				</Route>
       </Routes>
