77a57f96a98944c8a1bb43fe2c3233950ca407b3
Mark Reyes <mr@marklreyes.com>
2025-01-14 11:45:38 -0800

    Add Loading and Error states to cover sad path, refactor fetch unto an async await, move Vans components to its own directory

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react"
 import { Link, useSearchParams } from "react-router-dom"
+import { getVans } from "../../api"
 
 export default function Vans() {
     const [vans, setVans] = useState([]);
     const [searchParams, setSearchParams] = useSearchParams()
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
 	const typeFilter = searchParams.get("type")
 
     useEffect(() => {
-        fetch("/api/vans")
-            .then(response => response.json())
-            .then(data => setVans(data.vans))
-            .catch(error => console.error("Error fetching vans data:", error));
-    }, []);
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        loadVans()
+    }, [])
 
 	const selectedVans = typeFilter ? vans.filter(van => van.type.toLowerCase() === typeFilter) : vans
 
@@ -41,6 +53,14 @@ export default function Vans() {
             }
             return prevParams
         })
+    }
+
+	if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
     }
 
     return (
