796e70db9d647722845f9dd5e466677875d1191a
Mark Reyes <mr@marklreyes.com>
2025-01-14 11:56:54 -0800

    Set up Login Form, update absolute paths for about and vans to relative

--- index.jsx
+++ index.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
+import Login from "./pages/Login"
 import Layout from './components/Layout';
 import Reviews from './pages/Host/Reviews';
 import Income from './pages/Host/Income';
@@ -25,9 +26,10 @@ function App() {
       <Routes>
 				<Route path="/" element={<Layout />} >
 					<Route index element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/vans" element={<Vans />} />
-					<Route path="/vans/:id" element={<VanDetail />} />
+					<Route path="about" element={<About />} />
+					<Route path="vans" element={<Vans />} />
+					<Route path="vans/:id" element={<VanDetail />} />
+					<Route path="login" element={<Login />} />
 					<Route path="host" element={<HostLayout />}>
 						<Route index element={<Dashboard />} />
 						<Route path="income" element={<Income />} />
