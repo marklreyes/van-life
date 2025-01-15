8e8f810b8326107e97ca87fd6c4621e21f67d8fa
Mark Reyes <mr@marklreyes.com>
2025-01-15 13:54:00 -0800

    Retire getVans(id) from MirageJS call in favor of Firebase call getVans()

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore/lite"
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8S_3CFjxrGRSvVZe01Qq8lwpI5lAaESg",
@@ -13,6 +13,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+	const snapshot = await getDocs(vansCollectionRef)
+	const vans = snapshot.docs.map(doc => ({
+		...doc.data(),
+		id: doc.id
+	}))
+	return vans
+}
+
 // A function whose only purpose is to delay execution
 // for the specified # of milliseconds when used w/ `await`
 // e.g. inside an async function:
@@ -21,19 +32,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(() => resolve(), ms))
 }
 
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
+// export async function getVans(id) {
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
