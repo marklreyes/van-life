254b028dccdcc6bb39c19f741208b4c47715655b
Mark Reyes <mr@marklreyes.com>
2025-01-15 11:46:31 -0800

    For demo purposes, use localStorage to save value of login. Modify history stack to not have back button point to /host after logging in.

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -3,12 +3,14 @@ import { Navigate, Outlet } from "react-router-dom"
 
 export default function AuthRequired() {
     const authenticated = true
+    const isLoggedIn = localStorage.getItem("loggedin")
 
     if (!authenticated) {
         return (
             <Navigate
                 to="/login"
                 state={{message: "You must log in first"}}
+				replace
             />)
     }
     return <Outlet />
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,11 @@ import React from "react"
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+
+	function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
 	return (
 	  <header>
 		<Link className="site-logo" to="/">#VanLife</Link>
@@ -15,6 +20,7 @@ export default function Header() {
 					className="login-icon"
 				/>
             </Link>
+			<button onClick={fakeLogOut}>X</button>
 		</nav>
 	  </header>
 	)
