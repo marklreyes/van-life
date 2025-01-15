356d998b901ad2e9eb1b6723afafb1305d43fa79
Mark Reyes <mr@marklreyes.com>
2025-01-14 16:27:04 -0800

    Authentication setup for sad path

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,15 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate, useLocation } from "react-router-dom"
 import { loginUser } from "../api"
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
-    const location = useLocation()
+    const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
+	const [status, setStatus] = useState("idle")
+    const [error, setError] = useState(null)
+	const location = useLocation()
 
     function handleSubmit(e) {
         e.preventDefault()
+        setStatus("submitting")
         loginUser(loginFormData)
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
     }
 
     function handleChange(e) {
@@ -22,11 +34,15 @@ export default function Login() {
 
     return (
         <div className="login-container">
-			{
-				location.state?.message &&
-				<h3 className="login-first">{location.state.message}</h3>
-			}
+            {
+                location.state?.message &&
+                <h3 className="login-error">{location.state.message}</h3>
+            }
             <h1>Sign in to your account</h1>
+            {
+                error?.message &&
+                <h3 className="login-error">{error.message}</h3>
+            }
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -42,7 +58,14 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting"
+                        ? "Logging in..."
+                        : "Log in"
+                    }
+                </button>
             </form>
         </div>
     )
